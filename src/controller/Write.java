c711862265c5f80ec9b53e4508a36744d1d38303
immoskyl <rromain27@neuf.fr>
2016-12-11 21:46:04 +0000

    read feature working well

--- src/controller/Write.java
+++ src/controller/Write.java
@@ -1,9 +1,17 @@
 package controller;
 
+
+import cryptography.PigeonEncryption;
+
 /**
  * Created by immoskyl on 11/12/16.
  */
 public class Write implements FeatureStrategy {
+    private PigeonEncryption encryption;
+
+    //choices:
+    //generate pigeon key (with length and file name)
+    //encrypt file
 
     public void execute() {}
 }
