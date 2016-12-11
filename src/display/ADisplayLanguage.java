f1f063bf5e556fb8c2d5e4042c0e7994c30b92a0
immoskyl <rromain27@neuf.fr>
2016-12-11 13:46:00 +0000

    factory done

---
+++ src/display/ADisplayLanguage.java
@@ -0,0 +1,22 @@
+package display;
+
+/**
+ * Created by immoskyl on 11/12/16.
+ */
+public abstract class ADisplayLangugage {
+    private IDisplayType displayType;
+
+    public FrenchText(IDisplayType displayType) {
+        this.displayType = displayType;
+    }
+
+    public void display (String s) {
+        displayType.display(s);
+    } //display()
+
+    public static void ChooseLanguage () {
+        System.out.println("Choose your language:");
+        System.out.println("1: English");
+        System.out.println("2: French");
+    } //ChooseLanguage()
+}
--- src/display/ADisplayLangugage.java
+++
@@ -1,7 +0,0 @@
-package display;
-
-/**
- * Created by immoskyl on 11/12/16.
- */
-public class ADisplayLangugage {
-}
